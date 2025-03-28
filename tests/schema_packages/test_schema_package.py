c26e0bf677f1df35b63293c599da9574253605bc
aalbino2 <andrea.albino@hotmail.it>
2025-03-28 15:51:48 +0100

    introduce pytest fixture

--- tests/schema_packages/test_schema_package.py
+++ tests/schema_packages/test_schema_package.py
@@ -1,12 +1,15 @@
+import glob
 import os.path
 
+import pytest
 from nomad.client import normalize_all, parse
 
+test_files = glob.glob(os.path.join('tests', 'data', '*.archive.yaml'))
 EXPECTED_VALUE = 3.4
 
 
-def test_schema_package():
-    test_file = os.path.join('tests', 'data', 'test.archive.yaml')
+@pytest.mark.parametrize('test_file', test_files)
+def test_schema_package(test_file):
     entry_archive = parse(test_file)[0]
     normalize_all(entry_archive)
 
