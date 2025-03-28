d53aab6d59e3234508c223b656b461540a2f8411
aalbino2 <andrea.albino@hotmail.it>
2025-03-28 15:36:33 +0100

    lint

--- tests/schema_packages/test_schema_package.py
+++ tests/schema_packages/test_schema_package.py
@@ -4,6 +4,7 @@ from nomad.client import normalize_all, parse
 
 EXPECTED_VALUE = 3.4
 
+
 def test_schema_package():
     test_file = os.path.join('tests', 'data', 'test.archive.yaml')
     entry_archive = parse(test_file)[0]
