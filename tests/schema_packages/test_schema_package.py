d632ab1f82af149a069127a528800be4051a304b
Sathya Sai Seetharaman <s3.at.work@gmail.com>
2025-04-23 10:45:00 +0000

    update tests

--- tests/schema_packages/test_schema_package.py
+++ tests/schema_packages/test_schema_package.py
@@ -1,11 +1,16 @@
 import glob
 import os.path
 
+import numpy as np
 import pytest
 from nomad.client import normalize_all, parse
 
 test_files = glob.glob(os.path.join('tests', 'data', '*.archive.yaml'))
-EXPECTED_VALUE = 3.4
+EXPECTED_VALUE = [
+    [-16.0397, 0.0000, 0.0000],
+    [0.0000, -16.1160, 0.0000],
+    [0.0000, 0.0000, -48.9908],
+]
 
 
 @pytest.mark.parametrize('test_file', test_files)
@@ -13,4 +18,5 @@ def test_schema_package(test_file):
     entry_archive = parse(test_file)[0]
     normalize_all(entry_archive)
 
-    assert entry_archive.data.value.m == EXPECTED_VALUE
+    # assert entry_archive.data.value.m == EXPECTED_VALUE
+    assert np.array_equal(entry_archive.data.value.m, EXPECTED_VALUE)
