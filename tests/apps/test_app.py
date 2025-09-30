aebc09b1a77e138f97ad13dcd0a34e48cfb87716
Sathya-S3 <s3.at.work@gmail.com>
2025-09-30 11:19:16 +0000

    removed app test

--- tests/apps/test_app.py
+++
@@ -1,5 +0,0 @@
-def test_importing_app():
-    # this will raise an exception if pydantic model validation fails for th app
-    from nomad_nmr_schema.apps import nmr_app
-
-    assert nmr_app.app.label == 'NMR App'
