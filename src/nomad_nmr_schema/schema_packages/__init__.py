85db6c920c1ec8af7ce04c33a22fe8d9bd5749e7
jrudz <rudzinski@mpip-mainz.mpg.de>
2025-10-24 20:24:16 +0200

    revert import

--- src/nomad_nmr_schema/schema_packages/__init__.py
+++ src/nomad_nmr_schema/schema_packages/__init__.py
@@ -1,13 +1,13 @@
 from nomad.config.models.plugins import SchemaPackageEntryPoint
 from pydantic import Field
 
-from nomad_nmr_schema.schema_packages.schema_package import m_package
-
 
 class NmrSchemaEntryPoint(SchemaPackageEntryPoint):
     parameter: int = Field(0, description='Custom configuration parameter')
 
     def load(self):
+        from nomad_nmr_schema.schema_packages.schema_package import m_package
+
         return m_package
 
 
